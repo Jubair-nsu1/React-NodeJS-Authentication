5e74096cb299c1a701eb3d30dfb65361e3811ab6
jubair421 <a.k.m.ahmed@northsouth.edu>
2024-05-27 15:38:16 +0600

    update frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+//Pages
+import Register from './Pages/Register';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Register />} />
+          {/* <Route path="/login" element={<Login />} /> */}
+          
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- frontend/src/Pages/Register.js
+++ frontend/src/Pages/Register.js
@@ -1,26 +1,76 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Register = () => {
 
-  const [fullname,setFullname] = useState('');
-  const [email,setEmail] = useState('');
-  const [password,setPassword] = useState('');
-  const [confirmPassword,setConfirmPassword] = useState('');
-
-  async function SignUp(e) {
-    e.preventDefault();
   
+  const [input, setInput] = useState({
+    fullname: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+  });
+
+  const [error, setError] = useState({
+    username: '',
+    password: '',
+    confirmPassword: ''
+  })
+
+  const onInputChange = e => {
+ 
   }
+ 
+  const validateInput = e => {
+ 
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    if(fullname && email && password && confirmPassword) {  
+
+      if(password !== confirmPassword){
+        console.log('The passwords dont match')
+        return
+      }else{
+        const response  = await fetch('http://localhost:1337/api/register', {
+          method:'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            email,
+            fullname,
+            password
+          }),
+        })
+        const data = await response.json()
+        console.log(data);
+      }
+    
+    }
+  }
+
+  // const onVerifyNewPassword = () => {
+  //     if(password !== confirmPassword){
+  //       console.log('The passwords dont match')
+  //       return
+  //     }else{
+  //       console.log('Ok.')
+  //     }
+  // }
+
+// https://www.cluemediator.com/password-and-confirm-password-validation-in-react
 
   return (
     <div>
-        <h1>Register</h1><br/><br/>
-        <div class="container border shadow">
+        <h1>Register User</h1><br/><br/>
+        <div class="container-sm border shadow">
 
-          <form onSubmit={SignUp}>
-              <div class="form-row">
+          <form onSubmit={handleSubmit}>
+              <div class="form-row mt-3">
                 <div class="col mb-2">
-                    <input type="text" class="form-control" placeholder="Name" name="fullname" value={fullname} onChange={e => setFullname(e.target.value)} />
+                    <input type="text" class="form-control" placeholder="Name" name="fullname" value={input.fullname} onChange={onInputChange} onBlur={validateInput}/>
+                    {error.email && <span className='err'>{error.username}</span>}
                 </div>
                 <div class="col mb-2">
                     <input type="email" class="form-control" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)}/>
@@ -29,10 +79,10 @@ const Register = () => {
                     <input type="password" class="form-control" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)}/>
                 </div>
                 <div class="col mb-2">
-                    <input type="password" class="form-control" placeholder="Password" value={confirmPassword} onChange={e => setConfirmPassword(e.target.value)}/>
+                    <input type="password" class="form-control" placeholder="Password" value={confirmPassword} onChange={e => setConfirmPassword(e.target.value)} onBlur={onVerifyNewPassword} />
                 </div>
                 <div class="d-grid gap-2 col-6 mx-auto mb-3 mt-5">                                       
-                    <button style={{fontWeight:'bold'}} class="btn btn-success btn-lg" type="submit">&nbsp;&nbsp; Submit</button>                                       
+                    <button style={{fontWeight:'bold'}} class="btn btn-success btn" type="submit">&nbsp;&nbsp; Sign Up</button>                                       
                 </div>
               </div>
           </form>
